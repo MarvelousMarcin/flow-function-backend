f73d125c1706e66af4de8540095474cc45fba6dd
MarvelousMarcin <01159356@pw.edu.pl>
2023-05-28 15:24:25 +0200

    init + config

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from "express";
+
+const port = 8000;
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello from express");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
