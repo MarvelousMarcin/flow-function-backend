823d5fa835e121f2ad76be02229fbfe81c184ec6
MarvelousMarcin <01159356@pw.edu.pl>
2023-05-28 15:56:29 +0200

    prisma conifg

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import express, { Express, Request, Response } from "express";
-
+import { prisma } from "./prisma/client";
 const port = 8000;
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   res.send("hello from express");
 });
 
